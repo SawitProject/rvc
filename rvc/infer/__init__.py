1c070b2a999de22f67784902083b683e05520919
programer01 <festyus09@gmail.com>
2026-01-26 21:05:42 +0700

    Update __init__.py

--- rvc/infer/__init__.py
+++ rvc/infer/__init__.py
@@ -1,4 +1,15 @@
-from .cli import main as launch_cli
-from .infer import run_inference_script as mainsc
+"""
+RVC CLI Tool - A command-line interface for RVC voice conversion
+"""
+
+__version__ = "1.0.0"
+__author__ = "BF667"
+__email__ = ""
+
+from .cli import main, convert_audio, VoiceConverter
+from .infer import run_inference_script 
+
+__all__ = ['main', 'convert_audio', 'VoiceConverter', 'run_inference_script']
+
 
 
