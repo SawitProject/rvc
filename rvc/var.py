7ea3a4e8f411c9035e22549ec123e414e600cbf7
programer01 <festyus09@gmail.com>
2026-01-28 20:30:32 +0700

    Create var.py

---
+++ rvc/var.py
@@ -0,0 +1,25 @@
+
+
+method = [
+  "pm",
+  "dio",
+  "mangio-crepe-tiny",
+  "mangio-crepe-small", 
+  "mangio-crepe-medium", 
+  "mangio-crepe-large",
+  "mangio-crepe-full",
+  "crepe-tiny",
+  "crepe-small", 
+  "crepe-medium",
+  "crepe-large",
+  "crepe-full",
+  "fcpe", 
+  "djcm", 
+  "fcpe-legacy",
+  "rmvpe",
+  "rmvpe-legacy",
+  "harvest", 
+  "yin", 
+  "pyin", 
+  "swipe"
+]
